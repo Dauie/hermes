866f9c3484eaad48053b64c6ccf94043b680bade
scollet <origamillamaa@gmail.com>
2018-09-10 21:44:46 -0700

    parsing ip

--- src/parsing/handlers/h_ip.c
+++ src/parsing/handlers/h_ip.c
@@ -1,12 +1,97 @@
 
-int populate_ip()
+int populate_ip(t_job * workload, uint32_t ip)
 {
-
+    return (0);
 }
 
-int verify_range()
+int construct_ip(t_job * workload, uint32_t ** ip, int is_range)
 {
+    int         i;
+    int         j;
+    char **     ip_r;
+    uint32_t    ip_a;
+    uint32_t    ip_z;
 
+    if (ip == NULL) return (-1);
+
+    ip_a = 0;
+    ip_z = 0;
+    for (i = 0; ip[i]; i++)
+    {
+        if (i == 1)
+            (ip_z ^= ip_a),
+            (ip_a ^= ip_z),
+            (ip_z ^= ip_a);
+        for (j = 0; ip[i][j]; j++)
+        {
+            if (ip[i][j] >= 0 || ip[i][j] <= 255)
+                ip_z = ip[i][j] | (ip_z << 8);
+            else
+                return (-1);
+        }
+        if (ip_z < ip_a)
+            //FAILURE
+            //IP_Z !> IP_A
+            return (-1);
+        else
+        {
+            for (; ip_a < ip_z; ip_a++)
+                if (populate_ip(workload, ip_a) < 0)
+                    return (-1);
+        }
+        /* START DEBUG */
+        char *test;
+        test = inet_pton(AF_INET, ip_z, test, INET_ADDRSTRLEN);
+        printf();
+        /* END DEBUG */
+    }
+
+    if (ip_r) free(ip_r);
+
+    return (0);
+}
+
+uint32_t ** split_range(char * ips)
+{
+    char **     q;
+    int         len;
+    char **     range;
+    /* IP Range */
+    uint32_t ** ip_r;
+
+    len(strlen(ips));
+    if (memchr('.', ips, len))
+        if ((q = ft_strplit(ips, '.')) == NULL)
+            return (NULL);
+    else
+        return (NULL);
+
+    ip_r = (uint32_t**)calloc(2, sizeof(uint32_t * 4));
+    if (ip_r == NULL)
+        return (NULL);
+
+    for (i = 0; q[i]; i++)
+    {
+        if (memchr('-', ips, len))
+        {
+            if ((range = ft_strsplit(q[i], '-')) == NULL)
+                return (NULL);
+            if (inet_pton(AF_INET, range[0], ip_r[0][i]) == NULL ||
+                inet_pton(AF_INET, range[1], ip_r[1][i]) == NULL)
+                return (NULL)
+        }
+        else
+        {
+            if (inet_pton(AF_INET, q[0], ip_r[0][i]) == NULL)
+                return (NULL)
+        }
+    }
+
+    if (q) free(q);
+    if (range) free(range);
+    if (i != 4) return (NULL);
+
+    return (ip_r);
 }
 
 int h_ip(t_job * workload, char * args)
@@ -53,30 +138,24 @@ int h_ip(t_job * workload, char * args)
      */
 
     int         i;
-    char **     ip;
-    char *      ip_n;
+    char **     ips;
+    char *      ip;
     int         subnet;
 
     if (args == NULL) return (1);
 
     if (memchr('/', args, strlen(args)))
     {
-        ip = ft_strsplit(args, '/');
-        ip_n = ip[0];
+        ips = ft_strsplit(args, '/');
+        ip = ips[0];
+        subnet = ips[1];
     }
     else
     {
-        ip_n = args;
+        ip = args;
     }
 
-    if (populate_ip() < 0)
-        //inet_pton(AF_INET, ip_n, &ip_n) < 0)
-    {
-        if (verify_range() < 0)
+    if (construct_ip(workload, split_range(ip)) < 0)
             return (-1);
-        else
-            split_range();
-    }
-
     return (0);
 }
