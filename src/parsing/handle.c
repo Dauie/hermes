0e616ccbb5773bf7690eacacd87b1097f54f2b55
scollet <origamillamaa@gmail.com>
2018-09-12 00:02:22 -0700

    handle() no longer requires 5 variables

--- src/parsing/handle.c
+++ src/parsing/handle.c
@@ -1,22 +1,24 @@
 #include "../../hermes.h"
 
-int handle(char *opt, t_tab *TAB, char *arg, t_job *workload, int tab_entries)
+int handle(char *opt, t_tab *TAB, char *arg, t_job *workload)
 {
-    int i;
-    int len;
+    int     i;
+    int     len;
+    char *  result;
 
     i = -1;
+    result = NULL;
     len = strlen(opt);
     /* iterate TAB and look for the correct entry */
-    while (++i < tab_entries && result == NULL)
+    while (TAB[++i].name && result == NULL)
         result = strnstr(opt, TAB[i].name, len);
     if (result)
     {
-        if (&TAB[i].function != NULL)
+        if (arg != NULL)
             return (TAB[i].function(workload, arg));
         else
-            return (1);
+            return (TAB[i].function(workload));
     }
 
-    return (0);
+    return (-1);
 }
