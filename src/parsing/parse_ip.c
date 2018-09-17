ace9d68aa06a6ca85ef5ecb2edace5e1e51c5f1b
scollet <origamillamaa@gmail.com>
2018-09-16 20:42:33 -0700

    update

--- src/parsing/parse_ip.c
+++ src/parsing/parse_ip.c
@@ -11,7 +11,7 @@ t_ip4 *new_ip4(void) {
 int verify_ip(uint32_t *ip, char *ip_str) {
 	if (inet_pton(AF_INET, ip_str, ip) < 1)
 		return (0);
-	if (*ip <= 0 || *ip >= IP_MAX)
+	if (*ip > IP_MAX)
 		return (0);
 	return (1);
 }
@@ -25,9 +25,10 @@ int get_ip(uint32_t *ip, char *ip_str) {
 	return (SUCCESS);
 }
 
-void	set_ip4(t_ip4 *data, uint32_t ip, uint32_t cidr) {
+void	set_ip4(t_ip4 *data, uint32_t ip) {
+	printf("%i\n", data->addr);
 	memcpy(&data->addr, &ip, sizeof(uint32_t));
-	memcpy(&data->cidr, &cidr, sizeof(uint32_t));
+	printf("%i\n", data->addr);
 }
 
 long	get_cidr(char *cidr) {
@@ -50,7 +51,6 @@ long	get_cidr(char *cidr) {
 
 int		parse_ip(t_node **ip_list, char *args) {
 	uint32_t	ip;
-	char 		*cidr;
 	t_ip4		*data;
 	t_node		*node;
 	long		cidr_m;
@@ -61,29 +61,27 @@ int		parse_ip(t_node **ip_list, char *args) {
 		return (FAILURE);
 	if (!(node = new_node()))
 		return (FAILURE);
-	cidr_m = (uint32_t)NULL;
-	if ((cidr = memchr(args, '/', strlen(args))))
-			if (!(cidr_m = get_cidr(cidr)))
-				return (FAILURE);
 	if (get_ip(&ip, strsep(&args, "\n")) < 0)
 		return (FAILURE);
-	set_ip4(data, ip, (uint32_t)cidr_m);
-	set_node(node, data, sizeof(data));
+	set_ip4(data, ip);
+	set_node(node, &data, sizeof(data));
 
 	#ifdef TESTING
 	#endif
 
-	listadd_head(ip_list, node);
+	listadd_head(ip_list, &node);
 	return (SUCCESS);
 }
 
 #ifdef TESTING
 int main(void) {
-	int error;
-	t_node *head;
-	t_node *ip_list;
-	char input[20];
+	char 	*p_ip;
+	int 	error;
+	t_node 	*head;
+	t_node 	*ip_list;
+	char 	input[20];
 
+	p_ip = NULL;
 	ip_list = new_node();
 	printf("debugging started > options:\n"
 			"<i.p.ad.dr> < test ip\n"
@@ -94,10 +92,10 @@ int main(void) {
 		fgets(input, 20, stdin);
 		if (!memcmp("display", input, 7)) {
 			head = ip_list;
-			for (; ip_list->data; ip_list = ip_list->next)
-				printf("%i %i\n",
-					   ((t_ip4*)ip_list->data)->addr,
-					   ((t_ip4*)ip_list->data)->addr);
+			for (; ip_list->data; ip_list = ip_list->next) {
+				inet_ntop(AF_INET, &((t_ip4*)ip_list->data)->addr, p_ip, INET_ADDRSTRLEN);
+				printf("%s\n", p_ip);
+			}
 			ip_list = head;
 		} else if (memcmp("quit", input, 4) == 0 ||
 				memcmp("exit", input, 4) == 0) {
