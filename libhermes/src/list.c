99d6e0c090f9c637d48441910478e361905482de
scollet <origamillamaa@gmail.com>
2018-09-20 23:40:48 -0700

    pushing my test

--- libhermes/src/list.c
+++ libhermes/src/list.c
@@ -100,7 +100,6 @@ static t_node			*bst_rm_search(t_node **tree, t_node **parent,
 	return (NULL);
 }
 
-
 int			remove_node(t_node **tree, void *data, void *(*min)(t_node *),
 						int (*cmp)(void *, void *))
 {
@@ -152,6 +151,85 @@ void		delete_tree(t_node **tree)
 {
 	if (tree && *tree)
 	{
-
+        return;
 	}
 }
+
+#ifdef TESTING
+typedef struct  s_data {
+    int         value;
+}               t_data;
+
+void        print_tree(t_node *tree)
+{
+    if (tree && tree->data)
+    {
+        printf("current value -> %i\n", ((t_data*)tree->data)->value);
+        print_tree(tree->left);
+        print_tree(tree->right);
+    }
+    else
+        printf("Branch done!\n");
+    return;
+}
+
+void    *test_min(t_node *tree)
+{
+    t_data  *save;
+
+    if (!tree)
+        return (NULL);
+    save = (t_data*)memalloc(sizeof(t_data));
+    while (tree->left)
+        tree = tree->left;
+    memcpy(save, tree->data, sizeof(t_data));
+    return (save);
+}
+
+int     test_cmp(void *l, void* r)
+{
+    return ((((t_data*)l)->value < ((t_data*)r)->value) ? -1:
+            (((t_data*)l)->value > ((t_data*)r)->value) ? 1:0);
+}
+
+int     main(void)
+{
+    t_data      *data;
+    t_node      *node;
+    t_node      *tree;
+    char        input[20];
+
+    tree = new_node();
+    tree->data = (t_data*)memalloc(sizeof(t_data));
+    while (1) {
+		printf("\n> ");
+		fgets(input, 20, stdin);
+		if (!memcmp("print", input, 5)) {
+			print_tree(tree);
+		} else if (!memcmp("add node", input, 8)) {
+		    printf("enter a value: ");
+		    fgets(input, 20, stdin);
+		    node = new_node();
+            node->data = (t_data*)memalloc(sizeof(t_data));
+            ((t_data*)node->data)->value = atoi(input);
+            add_node(&tree, &node, test_cmp);
+        } else if (!memcmp("del node", input, 8)) {
+            printf("enter a value: ");
+            fgets(input, 20, stdin);
+            data = (t_data*)malloc(sizeof(t_data));
+            data->value = atoi(input);
+            remove_node(&tree, data, test_min, test_cmp);
+		} else if (!memcmp("del tree", input, 8)) {
+            delete_tree(&tree);
+		} else if (!memcmp("quit", input, 4) ||
+			!memcmp("exit", input, 4)) {
+			break;
+		} else {
+            printf("Invalid command");
+        }
+		fflush(stdin);
+	}
+	return (1);
+    return (0);
+}
+#endif
