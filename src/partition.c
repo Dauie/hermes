779224df77fa81b95a7357cd41fcd68b2b2a12b9
scollet <origamillamaa@gmail.com>
2018-10-03 21:56:56 -0700

    update partitioning

--- src/partition.c
+++ src/partition.c
@@ -1,10 +1,4 @@
-//
-// Created by Samuel Collet on 10/3/18.
-//
-
-//
-// Created by Samuel Collet on 10/3/18.
-//
+#include "../incl/hermes.h"
 
 void partition_ip4(t_node **src, t_node **w_jobs, int (*cmp)(void *, void *))
 {
@@ -12,7 +6,7 @@ void partition_ip4(t_node **src, t_node **w_jobs, int (*cmp)(void *, void *))
         return ;
     if (w_jobs.left)
         partition_ip4(src, &w_jobs.left, cmp);
-    add_node(&w_jobs->targets->ip4, &src->data, cmp);
+    add_node(&w_jobs->data->targets->ip4, &src->data, cmp);
     remove_node(src, src->data, cmp);
     if (w_jobs.right)
         partition_ip4(src, &w_jobs.right, cmp);
@@ -22,9 +16,9 @@ void partition_port(t_node **src, t_node **w_jobs, int (*cmp)(void *, void *))
 {
     if (!w_jobs)
         return ;
-    if (w_jobs.left)
-        partition_port(src, &w_jobs.left, cmp);
-    add_node(&w_jobs->scan_portlist->ports, &src->data, cmp);
+    if (w_jobs->left)
+        partition_port(src, &w_jobs->left, cmp);
+    add_node(&w_jobs->data->scan_portlist->ports, &src->data, cmp);
     remove_node(src, src->data, cmp);
     if (w_jobs.right)
         partition_port(src, &w_jobs.right, cmp);
@@ -37,7 +31,7 @@ t_node *split_tree(t_node **src, int threads)
     return (tree);
 }
 
-void partition_ip4rng(uint32_t parts, t_node **src, t_node **w_jobs, int (*cmp)(void *, void *))
+void partition_ip4rng(uint32_t parts, t_node **src, t_node **w_jobs)
 {
     t_node *temp;
     t_node *partitioned;
@@ -45,14 +39,14 @@ void partition_ip4rng(uint32_t parts, t_node **src, t_node **w_jobs, int (*cmp)(
     if (!w_jobs)
         return ;
     temp = NULL;
-    // TODO balanced split over number of nodes
-    partitioned = *split_nip4rng(parts, *src->data, ip4rng_cmp);
+    // NOTE : this /should/ work
+    partitioned = split_nip4rng(parts, *src->data, ip4rng_cmp);
     while (partitioned)
     {
         if (!temp)
-            temp = w_jobs;
-        add_node(&w_jobs->targets->iprange, &partitioned->data, cmp);
-        remove_node(&partitioned, partitioned->data, cmp);
+            temp = *w_jobs;
+        add_node(&w_jobs->data->targets->iprange, &partitioned->data, ip4rng_cmp, ip4rng_min);
+        remove_node(&partitioned, partitioned->data, ip4rng_cmp, ip4rng_min);
         temp = temp->next;
     }
     remove_node(&src, src->data, cmp);
@@ -66,36 +60,69 @@ void partition_portrng(uint32_t parts, t_node **src, t_node **w_jobs, int (*cmp)
     if (!w_jobs)
         return ;
     temp = NULL;
-    // TODO balanced split over number of nodes
-    partitioned = split_split_nprtrng(parts, &src, num_threads);
+    // NOTE : this /should/ work
+    partitioned = split_nprtrng(parts, &src, parts);
     while (partitioned)
     {
         if (!temp)
             temp = w_jobs;
-        add_node(&w_jobs->scan_portlist->port_range, &partitioned->data, cmp);
+        add_node(&w_jobs->data->ports->port_range, &partitioned->data, cmp);
         remove_node(&partitioned, partitioned->data, cmp);
         temp = temp->next;
     }
     remove_node(&src, src->data, cmp);
 }
 
-void	partition_work(t_job *job, t_workerlist *worker_list)
+int     job_cmp(void *lft, void *rgt)
 {
+    return (-1);
+}
+
+t_node *new_jobtree(uint32_t wrkr_cnt)
+{
+    uint32_t total;
+
+    total = 0;
+    if (!(job_tree = (t_node*)memalloc(sizeof(t_node))))
+        // hermes error
+        ;
+    if (!(job_tree->data = (t_job*)memalloc(sizeof(t_job))))
+        // hermes error
+        ;
+    if (!(job_tree->data->options = (t_ops*)memalloc(sizeof(t_ops))))
+        ;
+    if (!(job_tree->data->targets= (t_targetlist*)memalloc(sizeof(t_targetlist))))
+        ;
+    job_tree->data->ports;
+    job_tree->data->syn_ports;
+    job_tree->data->ack_ports;
+    job_tree->data->udp_ports;
+    while (total < worker_count)
+    {
+        add_node(&job_tree, job_tree->data, job_cmp);
+        total++;
+    }
+}
+
+t_node  *partition_jobs(t_job *job, uint32_t worker_count)
+{
+    uint32_t    total;
+    t_node      *job_tree;
+
+    job_tree = new_jobtree(worker_count);
     while (job->targets->ip)
         partition_ip4(&job->targets->ip,
-                      &w_jobs, ip4_cmp);
-
-    while (job->scan_portlist.ports)
-        partition_port(&job->scan_portlist.ports,
-                       &w_jobs, port_cmp);
-
+                      &job_tree, ip4_cmp);
+    while (job->ports.ports)
+        partition_port(&job->ports.ports,
+                       &job_tree, port_cmp);
     while (job->targets->iprange)
-        partition_ip4rng(worker_list->wrkr_count,
+        partition_ip4rng(worker_count,
                          &job->targets->iprange,
-                         &w_jobs, ip4rng_cmp);
-
-    while (job->scan_portlist.port_range)
-        partition_portrng(worker_list->wrkr_count,
-                          &job->scan_portlist.port_range,
-                          &w_jobs, portrng_cmp);
+                         &job_tree, ip4rng_cmp);
+    while (job->ports.prtrngs)
+        partition_portrng(worker_count,
+                          &job->ports.prtrngs,
+                          &job_tree, portrng_cmp);
+    return (job_tree);
 }
