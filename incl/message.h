64e84d8c21999ab7ea993038aaf55960be9028a6
dauie <rlutt@student.42.us.org>
2018-10-04 21:19:39 -0700

    locked makefile, pushing to merge with scollet

--- incl/message.h
+++ incl/message.h
@@ -2,10 +2,13 @@
 #define HERMES_MESSAGE_H
 
 /*
-**	Types: Job
+**	Types: Job, Error, Result
+ *
 ** 	Job Codes: JobOffer, JobAccept,
+ *
+*	Error Codes:
 **
-** 					General header layout
+**	General header layout
 **
 **    0                   1                   2                   3
 **    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
@@ -16,20 +19,30 @@
 **   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+...
 **
 **
-**				Job Offer
+**	Job Offer Example
 **
 **    0                   1                   2                   3
 **    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
 **   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-**   |               Type            |              Code             |
+**   |               Type   (0)     |              Code   (0)        |
 **   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-**   |                             Length                            |
+**   |                             Length  ex. (1754)                |
 **   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 */
 
 # define HERMES_MSG_MAX (64)
 
+/*
+**	Hermes Message Types
+*/
+# define MSGTYPE_JOB (0)
 
+/*
+**	Job Codes
+*/
+# define JOB_OFFER (0)
+# define JOB_ACCEPT (1)
+# define JOB_DENY (2)
 # define
 
 #endif //HERMES_MESSAGE_H
